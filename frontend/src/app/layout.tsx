b084388a4bc128cad713800c5503c6f30595b3d3
Pablo Teixeira <pablo.teixeira@pulsus.mobi>
2025-03-23 16:04:15 -0300

    feat(frontend): initial version

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Portfólio de Pablo Teixeira",
+  description: "Bem-vindo ao meu portfólio profissional. Aqui você encontrará uma coleção dos meus projetos e trabalhos em desenvolvimento de software, demonstrando minhas habilidades e experiências na área de tecnologia.",
+};
+
+
+const font = Montserrat({
+  subsets: ["latin"],
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body
+        className={`${font.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
