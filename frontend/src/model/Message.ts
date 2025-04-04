69fa31cda9e890daa986bb3c4d38a7e30360d5df
Pablo Teixeira <pablo.teixeira@pulsus.mobi>
2025-04-03 21:48:33 -0300

    bugfix(frontend): build adjusts

--- frontend/src/model/Message.ts
+++ frontend/src/model/Message.ts
@@ -1,7 +1,9 @@
+import { ReactNode } from "react";
+
 export default interface Message {
 	id: string;
 	text: string;
 	author: string;
 	side: "left" | "right";
-	icon?: any;
+	icon?: ReactNode;
 }
