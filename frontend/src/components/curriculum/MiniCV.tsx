7955259a1f1ccdfee45b96ff1e2d61a852f3b6b0
opabloteixeira <pablopst@gmail.com>
2025-04-08 21:02:40 -0300

    change perfil image

--- frontend/src/components/curriculum/MiniCV.tsx
+++ frontend/src/components/curriculum/MiniCV.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function MiniCV() {
 	return (
 		<div className="flex-1 flex flex-col-reverse gap-6 md:flex-row lg:flex-col-reverse xl:flex-row items-center md:items-start lg:items-center xl:items-start bg-black border border-zinc-800 rounded-2xl px-6 pt-6">
-			<div className="relative min-w-86 h-68 xl:self-end">
-				<Image className="grayscale-50" src="/perfil.png" alt="Foto de perfil" fill />
+			<div className="relative min-w-70 h-68 xl:self-end">
+				<Image className="grayscale-10" src="/perfil.png" alt="Foto de perfil" fill />
 			</div>
 			<div className="flex flex-col gap-5 self-center py-6 items-center  md:items-start lg:items-center xl:items-start">
 				<div className="flex flex-col items-center md:items-start lg:items-center xl:items-start">
